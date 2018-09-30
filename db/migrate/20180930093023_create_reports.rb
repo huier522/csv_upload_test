7bad616327c39df4c50ca95d434ed006f78663d4
huier <andychen522.ambit@gmail.com>
2018-09-30 18:33:00 +0800

    create Report fields

--- db/migrate/20180930093023_create_reports.rb
+++ db/migrate/20180930093023_create_reports.rb
@@ -1,7 +1,12 @@
 class CreateReports < ActiveRecord::Migration[5.2]
   def change
     create_table :reports do |t|
-
+      t.string :phone_num
+      t.text :rp_offer
+      t.date :expiration_date
+      t.date :trigger_date
+      t.date :init_date
+      t.string :init_ivr_code
       t.timestamps
     end
   end
