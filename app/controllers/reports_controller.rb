96fb5347db6cc55227016d7cc7708d2f515588a2
huier <andychen522.ambit@gmail.com>
2018-10-02 02:04:09 +0800

    add index and import action on Reports Controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,19 @@
 class ReportsController < ApplicationController
+
+  def index
+    @reports = Report.all
+    respond_to do |format|
+      format.html
+      format.csv { send_data @reports.to_csv(
+        ['phone_num', 'rp_offer', 'expiration_date', 'trigger_date', 
+        'init_date', 'init_ivr_code']
+      )}
+    end
+  end
+
+  def import
+    Report.import(params[:file])
+    redirect_to root_path, notice: 'CSV uploaded successfully'
+  end
+
 end
