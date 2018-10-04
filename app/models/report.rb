103529bc037e482396c31f8a0e421a1ba2116a7a
huier <andychen522.ambit@gmail.com>
2018-10-04 23:50:06 +0800

    define import file method on Report model by using roo gem

--- app/models/report.rb
+++ app/models/report.rb
@@ -13,16 +13,37 @@ class Report < ApplicationRecord
   # Import CSV, Find or Create Report by its phone number
   # Update the record
   def self.import(file)
-    CSV.foreach(file.path, headers: true) do |row|
-      reports_hash = row.to_hash
-      report = find_or_create_by!(phone_num: reports_hash['phone_num'], 
-        rp_offer: reports_hash['rp_offer'], 
-        expiration_date: reports_hash['expiration_date'], 
-        trigger_date: reports_hash['trigger_date'], 
-        init_date: reports_hash['init_date'], 
-        init_ivr_code: reports_hash['init_ivr_code'])
-      report.update_attributes(reports_hash)
+    spreadsheet = Roo::Spreadsheet.open(file.path)
+    header = spreadsheet.row(1)
+    # 以 model命名的欄位 存放到 header2 對應取代原資料的中文 header
+    # 就可以省略掉 report.phone_num = row["門號"]等程式碼
+    header2 = ["phone_num", "rp_offer", "expiration_date", 
+      "trigger_date", "init_date", "init_ivr_code"]
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header2, spreadsheet.row(i)].transpose]
+      report = find_by(phone_num: row["phone_num"]) || new
+      report.attributes = row
+      # report.phone_num = row["門號"]
+      # report.rp_offer = row["RP Offer"]
+      # report.expiration_date = row["上網期限到期日"]
+      # report.trigger_date = row["落地觸發日"]
+      # report.init_date = row["啟用日"]
+      # report.init_ivr_code = row["啟用IVR CODE"]
+      report.save!
     end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when ".csv" then 
+      Roo::CSV.new(file.path, nil, :ignore)
+    when ".xls" then 
+      Roo::Excel.new(file.path, nil, :ignore)
+    when ".xlsx" then 
+      Roo::Excelx.new(file.path, nil, :ignore)
+    else 
+      raise "Unknow file type: #{file.original_filename}"
+    end  
   end
 
 end
